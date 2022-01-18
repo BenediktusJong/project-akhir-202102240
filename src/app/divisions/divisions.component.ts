4b247bd6b735f037c58a2cdb500257ee25ba75d0
Admin <benediktusjong@gmail.com>
2022-01-18 15:51:37 +0700

    UAS_2

--- src/app/divisions/divisions.component.ts
+++ src/app/divisions/divisions.component.ts
@@ -7,7 +7,7 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./divisions.component.css']
 })
 export class DivisionsComponent implements OnInit {
-  url= "https://spa-api.aqiladigital.com/api/divisions"
+  url= "https://spa-api.aqiladigital.com/api/divisions/"
   li:any;
   constructor(private http : HttpClient) {
     this.http.get(this.url).subscribe(Response => {
@@ -18,7 +18,17 @@ export class DivisionsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  private tambahDivisi() {
-    this.http.post(this.url, {})
+  tambahDivisi(namaInput: string, descInput: string) {
+    this.http.post(this.url, {"name": namaInput, "description": descInput}).subscribe(data => {
+      console.log(data);
+    });
+    setTimeout(()=>{ window.location.reload()}, 5000)
+  }
+
+  hapusDivisi(id: number) {
+    this.http.delete(this.url + id).subscribe(data => {
+      console.log(data);
+    });
+    setTimeout(()=>{ window.location.reload()}, 5000)
   }
 }
